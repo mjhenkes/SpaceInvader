0422654533026d41a1095d4650df0ab7763379b4
Kevin.Schuster <Kevin.Schuster@cerner.com>
2012-05-06 18:47:14 -0500

    now used 5 missiles

--- SpaceInvaders/HelloWorldLayer.m
+++ SpaceInvaders/HelloWorldLayer.m
@@ -15,7 +15,13 @@
 
 CCSprite *turret;
 CCSprite *invader;
-CCSprite *projectile;
+CCSprite *projectile1;
+CCSprite *projectile2;
+CCSprite *projectile3;
+CCSprite *projectile4;
+CCSprite *projectile5;
+int projectileIndex = 0;
+
 
 #pragma mark - HelloWorldLayer
 
@@ -56,8 +62,20 @@ CCSprite *projectile;
         [invader setPosition:CGPointMake(midX, [[CCDirector sharedDirector] winSize].height -32)];
         [self addChild:invader];
         
-        projectile = [CCSprite spriteWithFile:@"projectile.png"];
-        [self addChild:projectile];
+        projectile1 = [CCSprite spriteWithFile:@"projectile.png"];
+        [self addChild:projectile1];
+        
+        projectile2 = [CCSprite spriteWithFile:@"projectile.png"];
+        [self addChild:projectile2];
+        
+        projectile3 = [CCSprite spriteWithFile:@"projectile.png"];
+        [self addChild:projectile3];
+        
+        projectile4 = [CCSprite spriteWithFile:@"projectile.png"];
+        [self addChild:projectile4];
+        
+        projectile5 = [CCSprite spriteWithFile:@"projectile.png"];
+        [self addChild:projectile5];
         
         [self schedule:@selector(nextFrame:)];
         
@@ -91,37 +109,104 @@ CCSprite *projectile;
 	[[app navController] dismissModalViewControllerAnimated:YES];
 }
 
-- (void) nextFrame:(ccTime)dt {
-    invader.position = ccp( invader.position.x + 100*dt, invader.position.y );
-    if (invader.position.x > [[CCDirector sharedDirector] winSize].width +32) {
-        invader.position = ccp( -32, invader.position.y );
+// Code to run for each frame executed
+- (void) nextFrame:(ccTime)dt 
+{
+    if (invader != nil)
+    {
+        // moves invader from left to right
+        invader.position = ccp( invader.position.x + 100*dt, invader.position.y );
+        if (invader.position.x > [[CCDirector sharedDirector] winSize].width +32) {
+            invader.position = ccp( -32, invader.position.y );
+        }
     }
     
-    CGRect projectileRect = CGRectMake(
-                                   projectile.position.x - (projectile.contentSize.width/2), 
-                                   projectile.position.y - (projectile.contentSize.height/2), 
-                                   projectile.contentSize.width, 
-                                   projectile.contentSize.height);
+    // move each projectile up if fired
+    [self moveProjectile:(projectile1)];
+    [self moveProjectile:(projectile2)];
+    [self moveProjectile:(projectile3)];
+    [self moveProjectile:(projectile4)];
+    [self moveProjectile:(projectile5)];
+}
 
-    CGRect invaderRect = CGRectMake(
-                                   invader.position.x - (invader.contentSize.width/2), 
-                                   invader.position.y - (invader.contentSize.height/2), 
-                                   invader.contentSize.width, 
-                                   invader.contentSize.height);
-
-    if (CGRectIntersectsRect(projectileRect, invaderRect)) 
+// moves the projectile upwards and destroys the invader if rects intersect
+- (void)moveProjectile:(CCSprite *)projectile
+{
+    if (projectile == nil)
     {
-        [self removeChild:invader cleanup:YES];
-        [self unschedule:_cmd];
+        return;
+    }
+
+    CGRect projectileRect = CGRectMake(
+                                       projectile.position.x - (projectile.contentSize.width/2), 
+                                       projectile.position.y - (projectile.contentSize.height/2), 
+                                       projectile.contentSize.width, 
+                                       projectile.contentSize.height);
+
+    if (invader != nil)
+    {
+        CGRect invaderRect = CGRectMake(
+                                         invader.position.x - (invader.contentSize.width/2), 
+                                         invader.position.y - (invader.contentSize.height/2), 
+                                         invader.contentSize.width,
+                                         invader.contentSize.height);
+        
+        // if the projectile and the invaders rect intersect, we have hit the invader and can remove it
+        if (CGRectIntersectsRect(projectileRect, invaderRect)) 
+        {
+            [self removeChild:invader cleanup:YES];
+            invader = nil;
+            [self unschedule:_cmd];
+        }
     }
 }
+
+// will return the next projectile to fire for the ship
+- (CCSprite *)getNextProjectile
+{
+    CCSprite *projectile = nil;
+    if (projectileIndex == 0)
+    {
+        projectile = projectile1;
+    }
+
+    if (projectileIndex == 1)
+    {
+        projectile = projectile2;
+    }
+
+    if (projectileIndex == 2)
+    {
+        projectile = projectile3;
+    }
+    
+    if (projectileIndex == 3)
+    {
+        projectile = projectile4;
+    }
+    
+    if (projectileIndex == 4)
+    {
+        projectile = projectile5;
+    }
+    
+    projectileIndex++;
+    if (projectileIndex == 5)
+    {
+        projectileIndex = 0;
+    }
+    
+    return projectile;
+}
+
 
 - (void) registerWithTouchDispatcher
 {
     CCDirector *director = [CCDirector sharedDirector];
 	[[director touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
 }
-                                         
+                    
+// when the user taps the screen, fire a missile and move the ship the correct direction
 - (BOOL)ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
     //ccTime time = 1;
@@ -137,24 +222,18 @@ CCSprite *projectile;
     return YES;
 }
 
+// fires a projectile
 - (void)fireProjectile
 {
-    //CCSprite *projectile = [CCSprite spriteWithFile:@"projectile.png"];
-    
     CGPoint turretPosition = [turret position];
     
+    CCSprite *projectile = [self getNextProjectile];
+    
     [projectile setPosition:turretPosition];
-    
-    
-    //[self addChild:projectile];
-    
+
     CGPoint endPoint = CGPointMake(turretPosition.x, 1000);
     
     [projectile runAction:[CCMoveTo actionWithDuration:1 position:endPoint]];
-    //[projectile runAction:[CCSequence actions:
-    //                       [CCMoveTo actionWithDuration:1 position:endPoint],
-    //                       [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
-    //                       nil]];
 }
 
 -(void)spriteMoveFinished:(id)sender {
